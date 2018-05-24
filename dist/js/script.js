b874771c0f7fea92c65959ddb8ddaae27c8ccbe8
Piotr Glejzer <glejzerpiotr@gmail.com>
2018-05-24 13:55:51 +0200

    added new files/gulp

---
+++ dist/js/script.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){for(var e=document.querySelectorAll(".about__h1"),t=0;t<e.length;t++)e[t].style.color="#000"});
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var hone = document.querySelectorAll('.about__h1');
+
+    for(var i=0; i<hone.length; i++){
+        hone[i].style.color ='#000';
+    }
+});
