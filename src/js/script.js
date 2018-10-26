9e6150d723cdcc853890a1abfb1a132db22bb554
pglejzer <p.glejzer@mdbootstrap.com>
2018-10-26 17:52:19 +0200

    clean repo

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var hone = document.querySelectorAll('.about__h1');
-
-    for(var i=0; i<hone.length; i++){
-        hone[i].style.color ='#000';
-    }
-});
